fd1528fb50ca925a50c9f537eb77990c140e1478
MaiCodes-exe <maise.b.costa@gmail.com>
2023-04-09 18:43:55 +0100

    added sliding function

--- app.js
+++ app.js
@@ -8,11 +8,18 @@ window.onload = function(){
 }
 
 function setGame(){
+    // board = [
+    //     [0, 0, 0, 0],
+    //     [0, 0, 0, 0],
+    //     [0, 0, 0, 0],
+    //     [0, 0, 0, 0]
+    // ]
+
     board = [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
+        [2, 2, 2, 2],
+        [2, 2, 2, 2],
+        [4, 4, 8, 8],
+        [4, 4, 8, 8]
     ]
     for(let r = 0;  r < rows; r++){
         for(let c = 0;  c < columns; c++){
@@ -40,4 +47,47 @@ if(num > 0){
 
     }
 }
+}
+
+
+document.addEventListener("keyup", (e) =>{
+    if(e.code == "ArrowLeft"){
+        slideEffect();
+    }
+})
+
+
+function filterZero(row){
+    return row.filter(num => num != 0);
+
+}
+function slide(row){
+    row = filterZero(row)
+    for (let i = 0; i< row.length-1 ; i++){
+        if (row[i] == row[i+1]){
+            row[i] *= 2;
+            row[i+1] = 0;
+            score += row[i];
+        }
+    }
+    row = filterZero(row);
+
+    while(row.length < columns){
+        row.push(0)
+    }
+    return row;
+}
+
+
+function slideEffect(){
+    for (let r = 0; r < rows; r++){
+        let row = board[r];
+        row = slide(row);
+        board[r] = row
+        for (let c = 0; c < columns; c++){
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c];
+            updateTile(tile, num)
+        }
+    }
 }
