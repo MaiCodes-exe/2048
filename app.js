6dc0aa6d22bdb258ccf8ad3a309fb0bca0c5e9fc
MaiCodes-exe <maise.b.costa@gmail.com>
2023-04-10 13:48:23 +0100

    corrected typo in index file

--- app.js
+++ app.js
@@ -36,57 +36,26 @@ function setGame(){
 }
 
 
-function hasEmptyTile(){
-    for (let r = 0; r < rows; r++){
-        for(let c = 0; c < columns; c++){
-            if (board[r][c] == 0){
-                return true;
-            }
-        }
-    }
-}
 
-function setTwo(){
-    if (!hasEmptyTile()){
-        return;
+function updateTile(tile, num) {
+    tile.innerText = "";
+    tile.classList.value = ""; //clear the classList
+    tile.classList.add("tile");
+    if (num > 0) {
+        tile.innerText = num.toString();
+        if (num <= 4096) {
+            tile.classList.add("x"+num.toString());
+        } else {
+            tile.classList.add("x8192");
+        }                
     }
-    let found = false;
-    while(!found){
-        let r = Math.floor(Math.random() * rows);
-        let c = Math.floor(Math.random() * columns);
-        if (board[r][c] == 0){
-            board[r][c] == 2;
-            let tile = document.getElementById(r.toString() + "-" + c.toString());
-            tile.innerText = "2";
-            tile.classList.add("x2");
-            found = true;
-
-        }
-    }
-}
-
-
-function updateTile(tile, num){
-tile.innerText = "";
-tile.classList.value = "";
-tile.classList.add("tile");
-if(num > 0){
-    tile.innerText = num;
-    if(num <= 4086) {
-        tile.classList.add("x"+num.toString());
-    }
-    else{
-        tile.classList.add("x8192");
-
-    }
-}
 }
 
 
 document.addEventListener("keyup", (e) =>{
     if(e.code == "ArrowLeft"){
-        slideEffect();
-        // setTwo();
+        slideLeft();
+        setTwo();
     }
     else if(e.code == "ArrowRight"){
         slideRight();
@@ -95,12 +64,12 @@ document.addEventListener("keyup", (e) =>{
     }
     else if(e.code == "ArrowUp"){
         slideUp();
-        // setTwo();
+        setTwo();
 
     }
     else if(e.code == "ArrowDown"){
         slideDown();
-        // setTwo();
+        setTwo();
 
     }
     document.getElementById("score").innerText = score;
@@ -111,36 +80,36 @@ function filterZero(row){
     return row.filter(num => num != 0);
 
 }
-function slide(row){
-    row = filterZero(row)
-    for (let i = 0; i< row.length-1 ; i++){
-        if (row[i] == row[i+1]){
+function slide(row) {
+    row = filterZero(row); 
+    for (let i = 0; i < row.length-1; i++){
+        if (row[i] == row[i+1]) {
             row[i] *= 2;
             row[i+1] = 0;
             score += row[i];
         }
-    }
+    } 
     row = filterZero(row);
-
-    while(row.length < columns){
-        row.push(0)
-    }
+   
+    while (row.length < columns) {
+        row.push(0);
+    } 
     return row;
 }
 
-
-function slideEffect(){
-    for (let r = 0; r < rows; r++){
+function slideLeft() {
+    for (let r = 0; r < rows; r++) {
         let row = board[r];
         row = slide(row);
-        board[r] = row
+        board[r] = row;
         for (let c = 0; c < columns; c++){
             let tile = document.getElementById(r.toString() + "-" + c.toString());
             let num = board[r][c];
-            updateTile(tile, num)
+            updateTile(tile, num);
         }
     }
 }
+
 
 
 function slideRight(){
@@ -148,8 +117,7 @@ function slideRight(){
         let row = board[r];
         row.reverse();
         row = slide(row);
-        row.reverse();
-        board[r] = row
+        board[r] = row.reverse();
         for (let c = 0; c < columns; c++){
             let tile = document.getElementById(r.toString() + "-" + c.toString());
             let num = board[r][c];
@@ -186,4 +154,37 @@ function slideDown(){
             updateTile(tile, num);
         }
     }
+}
+
+function setTwo(){
+    if (!hasEmptyTile()){
+        return;
+    }
+    let found = false;
+    while(!found){
+        let r = Math.floor(Math.random() * rows);
+        let c = Math.floor(Math.random() * columns);
+        if (board[r][c] == 0){
+            board[r][c] == 2;
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            tile.innerText = "2";
+            tile.classList.add("x2");
+            found = true;
+
+        }
+    }
+}
+
+
+
+function hasEmptyTile(){
+    let count = 0;
+    for (let r = 0; r < rows; r++){
+        for(let c = 0; c < columns; c++){
+            if (board[r][c] == 0){
+                return true;
+            }
+        }
+    }
+    return false;
 }
